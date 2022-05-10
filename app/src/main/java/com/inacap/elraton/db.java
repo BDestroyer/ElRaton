c4ab5152e4deaa929ad4719eef7b3729f9fbd681
BDestroyer <nkomb860@gmail.com>
2022-05-09 20:03:01 -0400

    commit

--- app/src/main/java/com/inacap/elraton/db.java
+++ app/src/main/java/com/inacap/elraton/db.java
@@ -13,14 +13,11 @@ public class db extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE usuario (email string PRIMARY KEY, password password not null, primerNombre string not null, segundoNombre string not null, apellidoPaterno string not null, apellidoMaterno string not null)");
         db.execSQL("CREATE TABLE producto(id int primary key autoincrement not null, descripcion string not null, precio int not null, cantidad int not null)");
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-        db.execSQL("drop table if exists usuario");
-        db.execSQL("CREATE TABLE usuario (email string PRIMARY KEY, password password not null, primerNombre string not null, segundoNombre string not null, apellidoPaterno string not null, apellidoMaterno string not null)");
         db.execSQL("drop table if exists producto");
         db.execSQL("CREATE TABLE producto(id int primary key autoincrement not null, descripcion string not null, precio int not null, cantidad int not null)");
         onCreate(db);
