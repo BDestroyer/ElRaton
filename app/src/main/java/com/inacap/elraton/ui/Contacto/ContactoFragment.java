6780eeac23c7f4dc86db9b7072ea72d854823c10
BDestroyer <nkomb860@gmail.com>
2022-05-19 21:46:32 -0400

    Commit

--- app/src/main/java/com/inacap/elraton/ui/Contacto/ContactoFragment.java
+++ app/src/main/java/com/inacap/elraton/ui/Contacto/ContactoFragment.java
@@ -4,17 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.inacap.elraton.R;
 
 public class ContactoFragment extends Fragment {
+    private EditText txtNombreContc,txtConsulta,txtCorreoContc;
+    private Button btnEnviar,btnLimpiar;
+    String NombreContc,CorreoContc,Consulta;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacto,container,false);
+        return inflater.inflate(R.layout.fragment_contacto, container, false);
+    }
+    @MainThread
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        txtNombreContc=view.findViewById(R.id.edtNombreContc);
+        txtCorreoContc=view.findViewById(R.id.edtCorreoContc);
+        txtConsulta=view.findViewById(R.id.edtConsulta);
+        btnEnviar=view.findViewById(R.id.btnEnviar);
+        btnLimpiar=view.findViewById(R.id.btnLimpiar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NombreContc=txtNombreContc.getText().toString();
+                CorreoContc=txtCorreoContc.getText().toString();
+                Consulta=txtConsulta.getText().toString();
+                if(Consulta.equals("") || NombreContc.equals("")|| CorreoContc.equals(""))
+                {
+                    Toast.makeText(getContext(), "Datos llenados", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(getContext(), "Debe ingresar los datos validos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
 }
--- app/src/main/java/com/inacap/elraton/ui/MainActivity.java
+++ app/src/main/java/com/inacap/elraton/ui/MainActivity.java
@@ -1,5 +1,4 @@
 package com.inacap.elraton.ui;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,19 +11,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import com.inacap.elraton.R;
 import com.inacap.elraton.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view)
             {
@@ -36,15 +34,11 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_Inicio, R.id.nav_Buscar, R.id.nav_Contacto)
-                .setOpenableLayout(drawer)
-                .build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_Inicio, R.id.nav_Buscar, R.id.nav_Contacto).setOpenableLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
