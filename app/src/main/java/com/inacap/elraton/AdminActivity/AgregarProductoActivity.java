91e2131640daa9c7e950e8339e83108504f2b8f0
BDestroyer <nko1999@hotmail.com>
2022-06-10 15:58:28 -0400

    commit

--- app/src/main/java/com/inacap/elraton/AdminActivity/AgregarProductoActivity.java
+++ app/src/main/java/com/inacap/elraton/AdminActivity/AgregarProductoActivity.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -23,9 +23,6 @@ import android.widget.Toast;
 import com.inacap.elraton.Metodo;
 import com.inacap.elraton.R;
 import com.inacap.elraton.db;
-
-import java.io.File;
-import java.io.FileOutputStream;
 
 public class AgregarProductoActivity extends AppCompatActivity {
 
--- app/src/main/java/com/inacap/elraton/ui/Inicio/InicioFragment.java
+++ app/src/main/java/com/inacap/elraton/ui/Inicio/InicioFragment.java
@@ -3,6 +3,7 @@ package com.inacap.elraton.ui.Inicio;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
